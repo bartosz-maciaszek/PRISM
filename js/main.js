db33401c0a31c7317e09643813c9578936892bb2
Andrew McCarthy <andrew@gidsy.com>
2013-06-27 00:33:13 +0200

    use freegeoip

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-var geo = myIP();
-
-$("#ip").text(geo[2]);
-$("#city").text(geo[1]);
-
-function myIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-    xmlhttp.send();
-
-    hostipInfo = xmlhttp.responseText.split("\n");
-    var ip = [];
-    console.log(hostipInfo);
-    for (i=0; i < 4; i++) {
-        ipAddress = hostipInfo[i].split(":");
-        ip.push(ipAddress[1]);
-    }
-
-    return ip;
-}
+$.get("http://freegeoip.net/json/", function(data) {
+    $("#ip").text(data.ip);
+    $("#city").text(data.city+", "+data.country_name);
+});
